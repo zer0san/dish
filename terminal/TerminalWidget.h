b59940061e3b16d84cbce358c98a8505e1cef87c
zer0san <1323639983@qq.com>
2026-05-28 16:13:06 +0800

    .

--- terminal/TerminalWidget.h
+++ terminal/TerminalWidget.h
@@ -29,8 +29,7 @@ signals:
 protected:
     void keyPressEvent(QKeyEvent *event) override;
     void mousePressEvent(QMouseEvent *event) override;
-    void mouseDoubleClickEvent(QMouseEvent *event) override;
-    void contextMenuEvent(QContextMenuEvent *event) override;
+
 
 private:
     void initStyle();
